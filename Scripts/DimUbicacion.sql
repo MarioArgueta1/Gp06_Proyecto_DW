9702b85163399c6e4477e4220e6f009374ea38af
DESKTOP-V5LMM6U\Mario <argueta.mario.e@gmail.com>
2024-09-22 17:26:28 -0600

    Script sql inicial para la consulta de llenado de DimUbicacion

---
+++ Scripts/DimUbicacion.sql
@@ -0,0 +1,26 @@
+SELECT sor.entity_id as 'IdUbicacionN', 
+CASE sor.country_id
+WHEN 'BZ' THEN 'Belice'
+WHEN 'CR' THEN 'Costa Rica'
+WHEN 'SV' THEN 'El Salvador'
+WHEN 'GT' THEN 'Guatemala'
+WHEN 'HN' THEN 'Honduras'
+WHEN 'NI' THEN 'Nicaragua'
+WHEN 'PA' THEN 'Panama'
+WHEN 'MX' THEN 'Mexico'
+ELSE 'Sin especificar'
+END AS 'Pais',
+sor.city as 'Ciudad',
+CONCAT(sor.street,',',sor.region) as 'Direccion',
+CASE so.order_currency_code
+WHEN 'BZD' THEN 'Dolar beliceño'
+WHEN 'CRC' THEN 'Colon costarricense'
+WHEN 'USD' THEN 'Dolar estadounidense'
+WHEN 'GTQ' THEN 'Quetzal'
+WHEN 'HNL' THEN 'Lempira hondureño'
+WHEN 'NIC' THEN 'Cordoba nicaraguense'
+WHEN 'PAB' THEN 'Balboa'
+WHEN 'MXN' THEN 'Peso mexicano'
+ELSE 'Sin especificar'
+END AS 'Moneda'
+from sales_order_address sor inner join sales_order so on so.entity_id = sor.parent_id where sor.address_type = "shipping"
